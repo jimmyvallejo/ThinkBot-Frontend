471341b39aa5f7993138653d8eef48935f287685
Jimmy <jrv1295@gmail.com>
2023-04-25 21:09:04 -0400

    Welcome page responsive

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,6 +5,8 @@ import { colors } from "../styles/colors";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
@@ -12,6 +14,10 @@ import { baseUrl } from "../services/baseUrl";
 
 export default function Welcome() {
   const navigate = useNavigate();
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+
 
   const { user, authenticateUser } = useContext(AuthContext);
 
@@ -28,19 +34,20 @@ export default function Welcome() {
   const blueColor = `rgba(30, 84, 183, 1)`;
 
   const but_style = {
-    width: "274px",
+    width: isSmallScreen ? "100px" : "274px",
     height: "60px",
-    fontSize: "24px",
+    fontSize: isSmallScreen ? "18px" : "24px",
     marginBottom: 20,
+   
   };
 
   const but2_style = {
-    width: "174px",
-    height: "40px",
-    fontSize: "20px",
+    width: isSmallScreen ? "120px" : "174px",
+    height: isSmallScreen ? "50px" : "40px",
+    fontSize:  isSmallScreen ? "14px" :" 20px",
     marginLeft: "5px",
     marginRight: "5px",
-    marginTop: "80px",
+    marginTop: isSmallScreen ? "1rem" : "80px",
   };
   const handleStudent = async () => {
     const login = {
@@ -80,6 +87,10 @@ export default function Welcome() {
     }
   }, [user]);
 
+
+
+
+
   return (
     <Grid container style={{ height: "100vh" }}>
       <Grid
@@ -93,14 +104,15 @@ export default function Welcome() {
           paddingTop: "4.3rem",
         }}
       >
-        <img src="./logo.svg" />
+        <img className="logoImg" src="./logo.svg" />
         <Typography
           variant="h1"
           align="center"
           style={{
-            fontSize: "64px",
+            fontSize: isSmallScreen ? "34px" : "64px",
             fontWeight: 700,
             color: "white",
+            paddingTop: isSmallScreen ? "3rem" : "0",
           }}
         >
           Thinkbot
@@ -117,9 +129,23 @@ export default function Welcome() {
         >
           Personal Student AI Tutor
         </Typography>
-        <img style={{ marginTop: "3rem" }} src="./mdc.svg" />
+        <img
+          style={{
+            marginTop: "3rem",
+            width: isSmallScreen ? "110px" : "175px",
+          }}
+          src="./mdc.svg"
+        />
       </Grid>
-      <Grid item xs={6}>
+      <Grid
+        item
+        xs={6}
+        style={{
+          background: isSmallScreen
+            ? "linear-gradient(180deg, #7B7B87 0%, #DFEFF1 33.33%, #1E54B7 66.67%, #23408F 100%)"
+            : "white",
+        }}
+      >
         <Box
           sx={{
             my: 8,
@@ -138,7 +164,7 @@ export default function Welcome() {
               fontWeight: 700,
             }}
           >
-            Welcome to
+            {isSmallScreen ? "" : "Welcome to"}
           </Typography>
           <Typography
             component="h1"
@@ -150,7 +176,7 @@ export default function Welcome() {
               color: colors.blues[500],
             }}
           >
-            ThinkBot
+            {isSmallScreen ? "" : "ThinkBot"}
           </Typography>
           <Typography
             component="h1"
@@ -161,7 +187,7 @@ export default function Welcome() {
               fontWeight: 500,
             }}
           >
-            Your personal AI tutor
+            {isSmallScreen ? "" : "Your personal AI tutor"}
           </Typography>
           <Typography
             component="h1"
@@ -174,17 +200,21 @@ export default function Welcome() {
               fontWeight: 600,
             }}
           >
-            Giving our students the world with the help of AI
+            {isSmallScreen
+              ? ""
+              : "Giving our students the world with the help of AI"}
           </Typography>
-          <Button style={but_style} onClick={() => navigate("/register")}>
-            Sign Up
-          </Button>
-          <Button
-            style={{ ...but_style, background: "white", color: "black" }}
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </Button>
+          <div className="firstButtons">
+            <Button style={but_style} onClick={() => navigate("/register")}>
+              Sign Up
+            </Button>
+            <Button
+              style={{ ...but_style, background: "white", color: "black" }}
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </Button>
+          </div>
         </Box>
         <div className="demos">
           <Button style={but2_style} onClick={() => handleStudent()}>
